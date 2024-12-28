ae51a58f5963a5c846ba7569312de934624cfee6
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-12-28 11:07:30 +0530

    feat: add frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex items-center justify-center'>
+      <h1>Welcome to alive!</h1>
+    </div>
+  )
+}
+
+export default App
