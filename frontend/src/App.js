4092c451beab200f9f65cacbc8bea73017516514
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-12-28 11:46:31 +0530

    feat; add tailwind css

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex justify-center items-center">
+       <h1>Frontend (CRA + tailwindCSS)</h1>
+    </div>
+  );
+}
+
+export default App;
